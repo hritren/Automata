63d7b6c2f2ba9f5a75e3dbb4cd73a230a2d2fb42
hritren <56520569+hritren@users.noreply.github.com>
2021-01-10 18:18:56 +0200

    encapsulated

--- Automata.h
+++ Automata.h
@@ -9,6 +9,7 @@ using namespace std;
 template <typename T>
 bool containsElement(const vector<T>& v, T el);
 bool areTheSame(vector<char> alphabet1, vector<char> alphabet2);
+bool operator==(const Rule& lhs, const Rule& rhs);
 
 class RegExpr;
 
@@ -21,14 +22,24 @@ struct Rule {
 		cout << init << " " << letter << " " << dest << endl;
 	}
 };
-bool operator==(const Rule& lhs, const Rule& rhs);
 
 class Automata {
 private:
 	size_t destinationDelta(size_t init, char letter) const;
 	int existsEpsilonRule(size_t init) const;
 	bool accepts(size_t tmpState, string word) const;
+	void addRule(size_t init, char letter, size_t dest);
+
 	bool acceptsEpsilon;
+	size_t startState;
+	size_t states;
+	string regExpr;
+
+	vector<char> alphabet;
+	vector<bool> finalStates;
+	vector<Rule> delta;
+	vector<Rule> epsilons;
+	Automata(const vector<char>& alphabet, size_t states, size_t startState, const vector<Rule>& delta, const vector<Rule>& epsilons, const vector<bool>& finalStates, bool acceptsEpsilon);
 public:
 	Automata static automataUnion(const Automata& left, const Automata& right);
 	Automata static intersection(const Automata& left, const Automata& right);
@@ -39,19 +50,5 @@ public:
 	bool acceptsWord(string word);
 	void print() const;
 
-	size_t startState;
-	size_t states;
-	string regExpr;
-
-	vector<char> alphabet;
-	vector<bool> finalStates;
-	vector<Rule> delta;
-	vector<Rule> epsilons;
-	Automata(const vector<char>& alphabet, size_t states, size_t startState, const vector<Rule>& delta, const vector<Rule>& epsilons, const vector<bool>& finalStates, bool acceptsEpsilon);
-	Automata();
 	Automata(const vector<char>& alphabet, char letter);
-
-	//for testing
-	void addRule(size_t init, char letter, size_t dest);
-
 };
